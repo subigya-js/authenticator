543f22d5eebadfb32ac24b90bfbf8fcc122f57af
SUBIGYA SUBEDI <developer.subigya@gmail.com>
2024-10-25 23:19:11 +0530

    Issue fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-import { Montserrat } from "next/font/google";
 import Register from "./components/Register/Register";
-
-const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function Home() {
   return (
